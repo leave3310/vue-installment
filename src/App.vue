91a3ae6725ade57756d74f3aa46cc58cff87cc2c
leave3310 <sampisam3@gmail.com>
2024-09-05 17:44:58 +0800

    init: 初始化專案

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { FwbDropdown, FwbListGroup, FwbListGroupItem } from 'flowbite-vue'
+
+</script>
+
+<template>
+  <fwb-dropdown text="Click me" placement="bottom">
+    <fwb-list-group>
+      <fwb-list-group-item>Item #1</fwb-list-group-item>
+      <fwb-list-group-item>Item #2</fwb-list-group-item>
+      <fwb-list-group-item>Item #3</fwb-list-group-item>
+    </fwb-list-group>
+  </fwb-dropdown>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+import flowbitePlugin from 'flowbite/plugin';
+
+
+export default {
+   content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}',
+    'node_modules/flowbite/**/*.{js,jsx,ts,tsx}'
+  ],
+  plugins: [
+      flowbitePlugin
+  ],
+  theme: {
+    extend: {},
+  },
+}
+
